01a61c5978282af5f32ad7e67abebc3d15c24fb7
deja.craft <dejacraft28@gmail.com>
2023-03-17 01:18:44 -0400

    userreducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useReducer } from "react";
+const App = () => {
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'INCREMENT':
+        return { ...state, counter: state.counter + 10 };
+      case 'DECREMENT':
+        return { ...state, counter: state.counter - 10 };
+      default:
+        return state;
+    }
+  };
+  // An action is a plain js object that has a type and may or may not have a payload
+  // Call the useReducer hook
+  const [state, dispatch] = useReducer(reducer, { counter: 0 });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <h1>Increase/Decrease by 10</h1>
+      <h2>{state.counter}</h2>
+      <div className="button-div">
+        <button
+          onClick={() => {
+            return dispatch({ type: 'INCREMENT' });
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          +
+        </button>
+        <button
+          onClick={() => {
+            return dispatch({
+              type: 'DECREMENT',
+            });
+          }}
+        >
+          -
+        </button>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
